f4a84eed57e3517354bc949bca712944b90e6b1b
ValentaCodes <valentacodes@gmail.com>
2023-02-21 23:47:58 -0700

    seed files for project

---
+++ seeds/seed.js
@@ -0,0 +1,21 @@
+const {User, Post }= require('../models')
+const sequelize = require('../config/connection')
+const userData = require('./userData.json')
+const postData = require('./postData.json')
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+  for (const post of postData) {
+    await Post.create({
+      ...post,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+  process.exit(0);
+};
+
+seedDatabase();
