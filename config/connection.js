4fab364bde9addaa392cf99e3d940de3a4888720
ValentaCodes <valentacodes@gmail.com>
2023-04-03 22:22:33 -0600

    Switched databse connection

--- config/connection.js
+++ config/connection.js
@@ -2,17 +2,15 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // This is connecting to our remote Database, If that can't establish a connection run locally. worse case, throw error
-// const sequelize = new Sequelize(
-//   process.env.MYSQLDATABASE,
-//   process.env.MYSQLUSER,
-//   process.env.MYSQLPASSWORD,
-//   {
-//     host: process.env.MYSQLHOST,
-//     dialect: 'mysql',
-//     port: process.env.MYSQLPORT,
-//   }
-// );
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'mysql',
-});
+const sequelize = new Sequelize(
+  process.env.MYSQLDATABASE,
+  process.env.MYSQLUSER,
+  process.env.MYSQLPASSWORD,
+  {
+    host: process.env.MYSQLHOST,
+    dialect: 'mysql',
+    port: process.env.MYSQLPORT,
+  }
+);
+
 module.exports = sequelize;
