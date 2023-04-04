381b7124a9ad867f07c9de7950074c2c672fce5f
ValentaCodes <valentacodes@gmail.com>
2023-04-03 22:20:09 -0600

    testing something

--- config/connection.js
+++ config/connection.js
@@ -2,15 +2,17 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // This is connecting to our remote Database, If that can't establish a connection run locally. worse case, throw error
-const sequelize = new Sequelize(
-  process.env.MYSQLDATABASE,
-  process.env.MYSQLUSER,
-  process.env.MYSQLPASSWORD,
-  {
-    host: process.env.MYSQLHOST,
-    dialect: 'mysql',
-    port: process.env.MYSQLPORT,
-  }
-);
-
+// const sequelize = new Sequelize(
+//   process.env.MYSQLDATABASE,
+//   process.env.MYSQLUSER,
+//   process.env.MYSQLPASSWORD,
+//   {
+//     host: process.env.MYSQLHOST,
+//     dialect: 'mysql',
+//     port: process.env.MYSQLPORT,
+//   }
+// );
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'mysql',
+});
 module.exports = sequelize;
